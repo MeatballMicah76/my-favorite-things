91ae1d7656a51ac5daf55fac08eb28434c827f17
Micah Malone <mmalone0075@myhta.org>
2018-02-20 15:04:33 -1000

    done

---
+++ app.js
@@ -0,0 +1,19 @@
+var btn1 = document.getElementById("button1");
+var btn2 = document.getElementById("button2");
+var btn3 = document.getElementById("button3");
+
+favoriteDesserts = ["A","b","C"];
+favoriteFoods = ["A","B", "C"];
+favoriteGames = ["A","B","C"];
+
+btn1.addEventListener("click", function(){
+
+});
+
+btn2.addEventListener("click", function(){
+
+});
+
+btn3.addEventListener("click", function(){
+
+});
